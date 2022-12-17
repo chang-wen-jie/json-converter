1e112f62857abefef15b751a75cb8cd51477d41b
unknown <changwenjie1999@gmail.com>
2022-12-17 21:49:36 +0100

    add download function

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,7 @@
-var fileInput = document.querySelector('#file-input');
-var table = document.createElement('table');
-var button = document.createElement('button');
+let fileInput = document.querySelector('#file-input');
+let table = document.createElement('table');
+let button = document.createElement('button');
+let csv = [];
 
 fileInput.addEventListener('change', (e) => {
     const file = e['target']['files'][0]
@@ -8,32 +9,29 @@ fileInput.addEventListener('change', (e) => {
 
     table.innerHTML = '';
     button.innerHTML = 'Downloaden';
+    csv = [];
 
     reader.addEventListener('load', (e) => {
         const json = e['target']['result'];
         const obj = JSON.parse(json);
-        let rows = [];
 
         Object.keys(obj).forEach((k) => {
-            rows.push(k, obj[k]);
-
-            var row = table.insertRow(-1);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
+            let row = table.insertRow(-1);
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
 
             cell1.innerHTML = k;
             cell2.innerHTML = obj[k];
-        });
-        const csvString = rows.join('\n');
-        const csvBlob = new Blob([csvString], {type: 'text/csv'});
-
-        button.addEventListener('click', (e) => {
-            console.log('clicked', e);
-            window.open(URL.createObjectURL(csvBlob), '_blank');
+            csv.push(k, obj[k]);
         });
     });
 
     document.body.appendChild(table);
     document.body.appendChild(button);
     reader.readAsText(file);
+});
+
+button.addEventListener('click', (e) => {
+    const csvBlob = new Blob([csv.join('\n')], {type: 'text/csv'});
+    window.open(URL.createObjectURL(csvBlob));
 });
