4d42e031f5fe90d83ec84c1e565b34e2e82526b9
unknown <changwenjie1999@gmail.com>
2022-12-19 16:49:17 +0100

    rm csv to json

--- scripts/content.js
+++ scripts/content.js
@@ -35,25 +35,3 @@ button.addEventListener('click', (e) => {
     const csvBlob = new Blob([csv.join('\n')], {type: 'text/csv'});
     window.open(URL.createObjectURL(csvBlob));
 });
-
-const csvR = "name,age,country\nJohn,30,USA\nMike,40,Canada\n";
-console.log(csvToJson(csvR));
-
-function csvToJson(csvR) {
-    const lines = csvR.split("\n");
-    const result = [];
-    const headers = lines[0].split(",");
-  
-    for (let i = 1; i < lines.length; i++) {
-      const obj = {};
-      const currentline = lines[i].split(",");
-  
-      for (let j = 0; j < headers.length; j++) {
-        obj[headers[j]] = currentline[j];
-      }
-  
-      result.push(obj);
-    }
-  
-    return JSON.stringify(result);
-  }
